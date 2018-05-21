e4bfcae26b580671e6ad56a34ed39900310b4a1c
Nicolai Ommer <nicolai.ommer@qaware.de>
2018-05-21 16:21:14 +0200

    Add paths for tigers website

--- src/environments/environment.tigers.ts
+++ src/environments/environment.tigers.ts
@@ -3,5 +3,8 @@ export const environment = {
   production: true,
   availableStatusWebSockets: new Map()
     .set('Field A', 'wss://tigers-mannheim.de/ssl-status/field-a/subscribe')
-    .set('Field B', 'wss://tigers-mannheim.de/ssl-status/field-b/subscribe')
+    .set('Field B', 'wss://tigers-mannheim.de/ssl-status/field-b/subscribe'),
+  availableVisionWebSockets: new Map()
+    .set('Field A', 'wss://tigers-mannheim.de/ssl-vision/field-a/subscribe')
+    .set('Field B', 'wss://tigers-mannheim.de/ssl-vision/field-b/subscribe'),
 };
