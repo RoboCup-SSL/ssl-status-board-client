055a5020281cfe063ed72d6cd19037c66083a065
Nicolai Ommer <nicolai.ommer@gmail.com>
2019-04-26 21:43:05 +0200

    Remove obsolete data classes

--- src/app/Command.ts
+++
@@ -1,9 +0,0 @@
-export class Command {
-  Id: number;
-  Name: string;
-
-  constructor() {
-    this.Id = -1;
-    this.Name = 'unknown command';
-  }
-}
--- src/app/Originator.ts
+++
@@ -1,9 +0,0 @@
-export class Originator {
-  Team: string;
-  BotId: number;
-
-  constructor() {
-    this.Team = 'TEAM_BLUE';
-    this.BotId = 1;
-  }
-}
